1b348755001eb61aff515ff957ca84fb2cb647d1
Ijarotimi Tomilayo <moladeijarotimi@gmail.com>
2022-04-26 02:27:00 +0100

    about and skillset sections

--- src/components/about-section/aboutsection.jsx
+++ src/components/about-section/aboutsection.jsx
@@ -1,12 +1,54 @@
 import React from "react";
+import "./aboutsection.scss";
 
 const AboutSection = () => {
   return (
-    <div>
-      AboutSection jjmm jjjjjmawr Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Culpa iure voluptate, excepturi esse in sapiente
-      doloremque maiores nulla natus est molestias porro minima voluptatum quasi
-      praesentium libero ducimus perspiciatis magnam!
+    <div className="aboutsection">
+      <div className="aboutsection_content1">
+        <p className="aboutsection-title">{"<about>"}</p>
+        <p className="aboutsection-item">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos totam
+          est, minima ea fugiat, explicabo repellendus vitae eius, quisquam ab
+          iure? Dolorem doloribus quae quasi ipsum alias fugit eligendi dolor.
+        </p>
+
+        <p className="aboutsection-item">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos totam
+          est, minima ea fugiat, explicabo repellendus vitae eius, quisquam ab
+          iure? Dolorem doloribus quae quasi ipsum alias fugit eligendi dolor.
+        </p>
+        <p className="aboutsection-item">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos totam
+          est, minima ea fugiat, explicabo repellendus vitae eius, quisquam ab
+          iure? Dolorem doloribus quae quasi ipsum alias fugit eligendi dolor.
+        </p>
+        <p className="aboutsection-title">{"</about>"}</p>
+      </div>
+      <div className="aboutsection_content2">
+        <p className="aboutsection-title">{"<skillset>"}</p>
+        <div className="skillset">
+          <i class="devicon-html5-plain"></i>
+          <i class="devicon-css3-plain"></i>
+          <i class="devicon-javascript-plain"></i>
+          <i class="devicon-react-original"></i>
+          <i class="devicon-redux-original"></i>
+          <i class="devicon-graphql-plain"></i>
+          <i class="devicon-github-original"></i>
+          <i class="devicon-heroku-original"></i>
+          <i class="devicon-firebase-plain"></i>
+          <i class="devicon-sass-original"></i>
+          <i class="devicon-solidity-plain"></i>
+          <i class="devicon-threejs-original"></i>
+          <i class="devicon-vscode-plain"></i>
+          <i class="devicon-yarn-plain"></i>
+          <i class="devicon-redhat-plain"></i>
+          <i class="devicon-materialui-plain"></i>
+          <i class="devicon-canva-original"></i>
+          <i class="devicon-jira-plain"></i>
+        </div>
+
+        <p className="aboutsection-title">{"</skillset>"}</p>
+      </div>
     </div>
   );
 };
--- src/components/fullpage/fullpage.jsx
+++ src/components/fullpage/fullpage.jsx
@@ -3,15 +3,18 @@ import "./fullpage.scss";
 import LandingSection from '../landing-section/landingpage-section';
 import ProjectSection from '../project-section/project-section';
 import QuoteSection from '../quotes/quote-section';
+import AboutSection from '../about-section/aboutsection';
 
 const FullPage = () => {
   return (
-      <div className='fullpage'>
-          <LandingSection />
-          <QuoteSection />
-          <ProjectSection/>
+    <div className="fullpage">
+      <LandingSection />
+      <QuoteSection />
+      <ProjectSection />
+      <QuoteSection />
+      <AboutSection />
     </div>
-  )
+  );
 }
 
 export default FullPage;
