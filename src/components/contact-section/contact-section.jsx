130941e63b67ec4830d82901db3951d4c2e3b827
Ijarotimi Tomilayo <moladeijarotimi@gmail.com>
2022-04-26 12:28:09 +0100

    completed the email contact section

--- src/components/contact-section/contact-section.jsx
+++ src/components/contact-section/contact-section.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 import "./contact-section.scss";
 
 const ContactSection = () => {
+  const SERVICE_ID = "service_hafopfd";
+  const TEMPLATE_ID = "template_ryb26uh";
+  const PUBLIC_KEY = "hr8fD49jZkSeyFnc-";
 
-    const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    }
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY).then(
+      (result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: "success",
+          title: "Message Sent Successfully",
+        });
+      },
+      (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: "error",
+          title: "Ooops, something went wrong",
+          text: error.text,
+        });
+      }
+    );
+    e.target.reset();
+  };
   return (
     <div className="contactsection">
       <p className="contactsection-title">{"<contact>"}</p>
@@ -25,20 +49,25 @@ const ContactSection = () => {
             type="text"
             className="contactsection-input"
             placeholder="NAME"
+            name="from_name"
           ></input>
           <input
             type="email"
             className="contactsection-input"
             placeholder="EMAIL"
+            name="from_email"
             required
           ></input>
           <input
             type="text"
             className="contactsection-input"
             placeholder="MESSAGE"
+            name="message"
             required
           ></input>
-          <button className="contactsection-submit">SUBMIT MESSAGE</button>
+          <button type="submit" value="send" className="contactsection-submit">
+            SUBMIT MESSAGE
+          </button>
         </form>
       </div>
 
